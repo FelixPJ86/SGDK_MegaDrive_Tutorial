b1479fd503253d82b03cb6f5996e5f8f905be71a
DESKTOP-DK47FL5\Casa <felixpotente@gmail.com>
2020-10-03 19:15:55 +0200

    hasta el 07

---
+++ 07 - Deconstruyendo escenarios/res/gfx.h
@@ -0,0 +1,9 @@
+#ifndef _RES_FONDOS_H_
+#define _RES_FONDOS_H_
+
+extern const Image fondo1;
+extern const Image fondo2;
+extern const Image fondo3;
+extern const Image fondo4;
+
+#endif // _RES_FONDOS_H_
---
+++ 07 - Deconstruyendo escenarios/res/gfx.res
@@ -0,0 +1,4 @@
+IMAGE fondo1 "fase1_1de4_dedia.png" BEST
+IMAGE fondo2 "fase1_2de4_anochece.png" BEST
+IMAGE fondo3 "fase1_3de4_casinoche.png" BEST
+IMAGE fondo4 "fase1_4de4_noche.png" BEST
